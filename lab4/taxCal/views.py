4e64e9970efbac7ee7b93ee7b516f59ca9380de5
MsharyAlsadoun <145196082+MsharyAlsadoun@users.noreply.github.com>
2023-10-09 21:20:05 +0300

    Update views.py

--- lab4/taxCal/views.py
+++ lab4/taxCal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render
+
+tax_rate = 0.15  
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def calculate_tax(request, number):
+    total_price = number + (number * tax_rate)
+    return render(request, 'calculate_tax.html', {'total_price': total_price})
+
+def tax_rate(request):
+    return render(request, 'tax_rate.html', {'tax_rate': tax_rate})
